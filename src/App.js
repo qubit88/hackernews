10865a177cab7ac62f33ed4d51662f0008c665d8
qubit88 <qubertec@gmail.com>
2019-05-09 12:00:35 +0300

    add hot module

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 function App() {
   let helloWorld = "Welcome to the Road to Learn React";
   let user = {
     firstName: "Qubit",
-    lastName: "88"
+    lastName: "Stark"
   };
   return (
     <div className="App">
