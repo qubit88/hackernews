d5b9f7d01660738fe98dc853f7af60ad34276057
qubit88 <qubertec@gmail.com>
2019-05-13 16:59:08 +0300

    snapshot test with enzime

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ onClick, className = "", children }) {
   return (
     <button onClick={onClick} className={className} type="button">
-      {children}
+      {children} hackernews
     </button>
   );
 }
